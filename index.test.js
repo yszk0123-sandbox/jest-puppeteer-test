233591c1e3d9a255aaf4c81a8781b396c3789a56
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-09-30 20:58:26 +0900

    add test for foo.html

--- index.test.js
+++ index.test.js
@@ -8,3 +8,10 @@ beforeAll(async () => {
 test('should display `test` text on page', async () => {
   await expect(page).toMatch('test');
 });
+
+test('should go to foo', async () => {
+  await expect(page).toClick('a[href^="/foo"]');
+  await page.waitForNavigation();
+  await expect(page).toMatch('foo');
+  await page.screenshot({ path: '.tmp/foo.png' });
+});
